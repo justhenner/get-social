671a209cad662bdd8a5d8bf13e05b58eedba0568
henner espinoza <hennerespinoza@henners-Air.attlocal.net>
2023-03-14 20:01:19 -0400

    routes updated

---
+++ routes/api/userRoute.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    deleteUser,
+    updateUser,
+    deleteUserAndThoughts,
+    addFriend,
+    removeFriend,
+} = require('../../controllers/userController');
+
+router.route('/').get(getUsers).post(createUser);
+
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+router.route('/:userId/thoughts/:thoughtId').delete(deleteUserAndThoughts);
+
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+
+module.exports = router;
